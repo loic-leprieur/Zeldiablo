c4a7122438f7fbe764691d64621927918c32d068
Bertrand Muller <bertrand.muller3@etu.univ-lorraine.fr>
2015-05-27 15:09:00 +0200

    Ecriture de la classe Moteur avec la méthode main() et demandeutilisateur() + Ecriture de la javadoc dans la classe Moteur

--- projet_zeldiablo/src/Moteur.java
+++ projet_zeldiablo/src/Moteur.java
@@ -1,9 +1,47 @@
+import java.util.Scanner;
 
+
+/**
+ * Classe qui execute un jeu
+ */
 public class Moteur {
 
+	/**
+	 * Methode principale
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Creation du jeu
+		Jeu j = new Jeu();
+		
+		//Demande d'une commande a executer a l'utilisateur
+		String dir = demandeUtilisateur();
+		
+		//Demande au jeu de deplacer le heros
+		j.deplaceHeros(dir);
 
+	}
+	
+	
+	/**
+	 * Methode qui permet de demander une direction a l'utilisateur
+	 * 
+	 * @return
+	 * 			Commande a executer
+	 */
+	public static String demandeUtilisateur() {
+		
+		Scanner sc = new Scanner(System.in);
+		String dir;
+		
+		System.out.print("Quel commande souhaitez-vous exécuter ? : ");
+		dir = sc.nextLine();
+		System.out.println("");
+		
+		return dir;
+		
 	}
 
 }
