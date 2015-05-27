454f5500be5c5defa2d6e5d8135efd6c37a17081
Florian LEDOUX <florian.ledoux1@etu.univ-lorraine.fr>
2015-05-27 15:08:12 +0200

    Création de la méthode deplaceHeros dans la classe Jeu

--- projet_zeldiablo/src/Jeu.java
+++ projet_zeldiablo/src/Jeu.java
@@ -15,6 +15,42 @@ public class Jeu {
  	
 	
 	
+	public void deplaceHeros(String dir){
+		
+		
+		switch(dir){
+		
+		case "H":
+			
+			heros.seDeplacer(0,10);
+			break;
+		
+		case "B":
+			heros.seDeplacer(0,-10);
+			break;
+			
+		case "G":
+			heros.seDeplacer(-10,0);
+			break;
+			
+		case "D":
+			heros.seDeplacer(10,0);
+			break;
+			
+		case "exit":
+			System.exit(0);
+			
+		default:
+			System.out.println("Commande non reconnue !");
+			break;
+		
+		
+		}
+		
+		
+		
+		
+	}
 	
 	
 	
