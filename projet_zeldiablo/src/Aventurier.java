845fa9779edb40fa49655b3ba3b2bfc2b63db98c
donleprieur <donleprieur@outlook.fr>
2015-05-27 15:05:08 +0200

    Implementation des deplacements de Aventurier

--- projet_zeldiablo/src/Aventurier.java
+++ projet_zeldiablo/src/Aventurier.java
@@ -1,4 +1,43 @@
 
 public class Aventurier {
+	// position en abscisse
+	private int posX;
+	// position en ordonnee
+	private int posY;
+	
+	/**
+	 * constructeur par defaut
+	 * initilialise la position
+	 * de l'aventurier
+	 */
+	public Aventurier(){
+		posX=0;
+		posY=0;
+	}
+	
+	/**
+	 * translation du personnage
+	 * @param x
+	 * @param y
+	 */
+	public void seDeplacer(int x, int y){
+		posX+=x;
+		posY+=y;
+	}
 
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
 }
