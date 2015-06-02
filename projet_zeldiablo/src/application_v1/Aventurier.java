1d1472ec9d4553bc9d1b6d7a4f49c2d520249761
donleprieur <donleprieur@outlook.fr>
2015-06-02 10:35:19 +0200

    Rajout temporaire de la version 1

---
+++ projet_zeldiablo/src/application_v1/Aventurier.java
@@ -0,0 +1,54 @@
+package application_v1;
+
+
+public class Aventurier {
+	// position en abscisse
+	private int posX;
+	// position en ordonnee
+	private int posY;
+	
+	/**
+	 * constructeur par defaut
+	 * initilialise la position
+	 * de l'aventurier
+	 */
+	public Aventurier(){
+		posX=5;
+		posY=5;
+	}
+	
+	/**
+	 * translation du personnage
+	 * @param x
+	 * @param y
+	 */
+	public void seDeplacer(int x, int y){
+		posX+=x;
+		posY+=y;
+	}
+	
+	/**
+	 * affichage des infos
+	 * d'un aventurier
+	 */
+	@Override
+	public String toString() {
+		return "Aventurier [posX=" + posX + ", posY=" + posY + "]";
+	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+}
