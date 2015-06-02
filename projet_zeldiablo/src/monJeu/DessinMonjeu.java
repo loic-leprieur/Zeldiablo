457b900c1c68784dc976029df5829e96c110aa4b
donleprieur <donleprieur@outlook.fr>
2015-06-02 11:03:10 +0200

    Ajout de la classe DessinMonJeu

---
+++ projet_zeldiablo/src/monJeu/DessinMonjeu.java
@@ -0,0 +1,15 @@
+package monJeu;
+import java.awt.image.BufferedImage;
+
+import moteurJeu.DessinJeu;
+
+
+public class DessinMonjeu implements DessinJeu {
+
+	@Override
+	public void dessiner(BufferedImage image) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
