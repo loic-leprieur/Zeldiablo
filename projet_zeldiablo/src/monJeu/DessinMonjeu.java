d4e7a8b742825b2733570142a953651cc58b1142
donleprieur <donleprieur@outlook.fr>
2015-06-02 11:49:45 +0200

    Implementaton dessinMonJeu

--- projet_zeldiablo/src/monJeu/DessinMonjeu.java
+++ projet_zeldiablo/src/monJeu/DessinMonjeu.java
@@ -1,14 +1,67 @@
 package monJeu;
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
+import monJeuExemple.MonJeu;
+import monJeuExemple.Personnage;
 import moteurJeu.DessinJeu;
 
 
 public class DessinMonjeu implements DessinJeu {
+	/**
+	 * constante pour gerer la taille des cases
+	 */
+	private static int TAILLE_CASE = 25;
 
+	/**
+	 * lien vers le jeu a afficher
+	 */
+	private MonJeu jeu;
+
+		
+	/**
+	 * appelle constructeur parent
+	 * 
+	 * @param j
+	 *            le jeutest a afficher
+	 */
+	public DessinMonjeu(MonJeu j) {
+		this.jeu = j;
+	}
+
+	/**
+	 * dessiner un objet consiste a dessiner sur l'image suivante methode
+	 * redefinie de Afficheur
+	 */
+	private void dessinerObjet(String s, int x, int y, BufferedImage im) {
+		Graphics2D crayon = (Graphics2D) im.getGraphics();
+		switch (s) {
+		case "PJ":
+			crayon.setColor(Color.blue);
+			crayon.fillOval(x * TAILLE_CASE, y * TAILLE_CASE, TAILLE_CASE,
+					TAILLE_CASE);
+			break;
+		case "MUR":
+			crayon.setColor(Color.gray);
+			crayon.fillRect(x * TAILLE_CASE, y * TAILLE_CASE, TAILLE_CASE,
+					TAILLE_CASE);
+			break;
+		default:
+			throw new AssertionError("objet inexistant");
+		}
+	}
+	
+	public void DessinMonJeu(MonJeu j) {
+		this.jeu = j;
+	}
+	
 	@Override
 	public void dessiner(BufferedImage image) {
-		// TODO Auto-generated method stub
+		// no sait que c'est un jeuTest
+		MonJeu j = (MonJeu) jeu;
+		Personnage pj = j.getPj();
+		this.dessinerObjet("PJ", pj.x, pj.y, image);
 		
 	}
 
