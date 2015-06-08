b3b503aeca7c6ab60160cf9e400d80fd378a1764
Bertrand Muller <bertrand.muller3@etu.univ-lorraine.fr>
2015-06-08 16:12:00 +0200

    Ajout de la méthode de test test_Monstre_NonDeplacementJoueur() dans la classe TestsDeplacementAleatoireMonstre

--- projet_zeldiablo/src/tests/TestsDeplacementAleatoireMonstre.java
+++ projet_zeldiablo/src/tests/TestsDeplacementAleatoireMonstre.java
@@ -39,4 +39,30 @@ public class TestsDeplacementAleatoireMonstre {
 		
 	}
 
+	
+	@Test
+	/**
+	 * Methode de test permettant de tester qu'un monstre se deplace aleatoirement 
+	 * lorsqu'une action, autre que le deplacement du joueur, est executee
+	 */
+	public void test_Monstre_NonDeplacementJoueur() {
+		
+		//Preparation des donnees
+		MonJeu mj = new MonJeu(11);
+		Monstre m = mj.getPj(1);
+		Commande c = new Commande();
+		/*c.frapper = true;*/
+		int posXInit = m.x;
+		int posYInit = m.y;
+		boolean deplacement = false;
+		
+		//Methode testee
+		mj.evoluer(c);
+		deplacement = ((m.x != posXInit) || (m.y != posYInit));
+		
+		//Validation des resultats
+		assertTrue("Le monstre devrait avoir change de position", deplacement);
+		
+	}
+	
 }
