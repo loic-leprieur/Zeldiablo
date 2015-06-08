2fb12bc77858e8a6dd0e3a807c58427e38782b7d
Bertrand Muller <bertrand.muller3@etu.univ-lorraine.fr>
2015-06-08 16:07:53 +0200

    Création de la méthode de test test_Monstre_DeplacementJoueur()

--- projet_zeldiablo/src/tests/TestsDeplacementAleatoireMonstre.java
+++ projet_zeldiablo/src/tests/TestsDeplacementAleatoireMonstre.java
@@ -1,14 +1,42 @@
 package tests;
 
 import static org.junit.Assert.*;
+import monJeu.MonJeu;
+import monJeu.Monstre;
+import moteurJeu.Commande;
 
 import org.junit.Test;
 
+
+/**
+ * Classe de test testant les deplacements aleatoires d'un monstre
+ */
 public class TestsDeplacementAleatoireMonstre {
 
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	/**
+	 * Methode de test permettant de tester qu'un monstre 
+	 * se deplace aleatoirement lorsque le heros se deplace
+	 */
+	public void test_Monstre_DeplacementJoueur() {
+		
+		//Preparation des donnees
+		MonJeu mj = new MonJeu(11);
+		Monstre m = mj.getPj(1);
+		Commande c = new Commande();
+		c.haut = true;
+		int posXInit = m.x;
+		int posYInit = m.y;
+		boolean deplacement = false;
+		
+		//Methode testee
+		mj.evoluer(c);
+		deplacement = ((m.x != posXInit) || (m.y != posYInit));
+		
+		//Validation des resultats
+		assertTrue("Le monstre devrait avoir change de position", deplacement);
+		
 	}
 
 }
