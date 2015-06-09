daa4b5672b79c5b9e7fefcc5dfd36c47800359ce
Florian LEDOUX <florian.ledoux1@etu.univ-lorraine.fr>
2015-06-09 13:41:39 +0200

    Passage des coordonnées du personnage en private + ajout getters et setters

--- projet_zeldiablo/src/monJeu/Personnage.java
+++ projet_zeldiablo/src/monJeu/Personnage.java
@@ -12,8 +12,8 @@ public abstract class Personnage {
 	/**
 	 * position en X et y
 	 */
-	public int x;
-	public int y;
+	private int x;
+	private int y;
 
 	/**
 	 * deplacer le personnage dans une direction
@@ -80,7 +80,59 @@ public abstract class Personnage {
 		
 	}
 	
+	/**
+	 * 
+	 * Getter de l'atribut x du personnage
+	 * 
+	 * @return 
+	 * 
+	 * 			coordonné x du personnage
+	 */
 	
+	public int getX() {
+		return x;
+	}
+
+
+	/**
+	 * Setter de l'atribut x du personnage
+	 * 
+	 * @param x 
+	 * 
+	 * 			nouvelle coordoné en x du personnage
+	 */
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+
+
+	/**
+	 * 
+	 * Getter de l'atribut y du personnage
+	 * 
+	 * @return 
+	 * 			coordonné y du personnage
+	 */
+	public int getY() {
+		return y;
+	}
+
+
+	/**
+	 * 
+	 * Setter de l'atribut x du personnage
+	 * 
+	 * @param y
+	 * 
+	 *			 nouvelle coordoné en x du personnage
+	 */
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+
+
 	/**
 	 * Methode retournant les caracteristiques d'un personnage
 	 */
