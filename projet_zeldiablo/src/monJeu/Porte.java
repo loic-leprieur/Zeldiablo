36b8fc7c559f6a2c8bd2e09d5dd0de95af924190
trivino2u <laura.trivino5@etu.univ-lorraine.fr>
2015-06-09 16:54:28 +0200

    Classe porte.

---
+++ projet_zeldiablo/src/monJeu/Porte.java
@@ -0,0 +1,23 @@
+package monJeu;
+
+public class Porte extends Declencheur{
+
+	public Porte(int posX, int posY, boolean etat) {
+		super(posX, posY, etat);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void declencherEffet(Personnage pers) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
